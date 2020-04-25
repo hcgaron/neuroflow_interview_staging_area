8ae156d8cbe4a80e43d28054b821d136f6af7107
Hans <hcgaron@gmail.com>
2020-04-25 14:29:21 -0400

    Added current streak to body of response when mood is created.

--- src/mood/api/views.py
+++ src/mood/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import permissions
 from mood.models import Mood
 from authentication.models import CustomUser, Profile
@@ -20,9 +21,13 @@ class MoodView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         instance = serializer.save(user=self.request.user)
-        print(self.request.user)
-        profile = Profile.objects.all()
-        print(profile)
-        print(self.request.user.profile)
-        # instance['streak'] = self.request.user.profile.streak
         return instance
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        response = Response(
+            {**serializer.data, 'streak': self.request.user.profile.current_streak}, status=status.HTTP_201_CREATED, headers=headers)
+        return response
